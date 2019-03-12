932ed76746a2e31fb6334c0b28532e5de1b2e683
toki23 <NULL>
2019-03-12 14:55:02 +0900

    英語死すべし

--- routes/helo.js
+++ routes/helo.js
@@ -3,7 +3,7 @@ var date_utils = require('date-utils');
 
 var fs = require('fs');
 var router = express.Router();
-var usidata = {
+var cowdata = {
     '1': 'non',
     '2': 'non',
     '3': 'non',
@@ -18,32 +18,19 @@ router.get('/', function(req, res, next) {
 });
 router.post('/', function(req, res, next) {
     var dt = new Date();
-    var usibg = req.body['usibang'];
-    var gpsdata = req.body['gps'];
+    var cowid = req.body['cowbang'];
+    var latitude = req.body['latitude'];
+    var longitude = req.body['longitude'];
     var formatted = dt.toFormat("YYYY MM DD HH24 MI SS");
-     switch(usibg){
-      case '1':
-      fs.appendFile('./usiz/c1.txt',gpsdata +'\t'+ formatted + '\n',function(err){});
-      break;
-       case '2':
-      fs.appendFile('./usiz/c2.txt',gpsdata +'\t'+ formatted + '\n',function(err){});
-      break;
-       case '3':
-      fs.appendFile('./usiz/c3.txt',gpsdata +'\t'+ formatted + '\n',function(err){});
-      break;
-       case '4':
-      fs.appendFile('./usiz/c4.txt',gpsdata +'\t'+ formatted, +'\n',function(err){});
-      break;
-      default:
-      console.log('miss');
-    }
     
-    usidata[usibg] = gpsdata;
-    usidata[usibg + 't'] = formatted;
+    fs.appendFile('./cowz/c1.txt',latitude + ' , '+ longitude +' , '+cowid,function(err){});
+    
+    cowdata[cowid] = gpsdata;
+    cowdata[cowid + 't'] = formatted;
     
     res.render('helo');
-    module.exports.data = usidata;
+    module.exports.data = cowdata;
 
 });
 module.exports = router;
-module.exports.data = usidata;
+module.exports.data = cowdata;
