a0d0863f0f47d82b71bab671a0d61b34678d1a25
toki23 <NULL>
2019-03-22 10:57:17 +0900

    dhakhdoahda

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.get('/get',(req,res) => res.send('/get'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
