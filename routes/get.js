55bec52002d2e0b00c2dbd9534020c53fd2d610c
toki23 <NULL>
2019-03-25 11:22:28 +0900

    ses

--- routes/get.js
+++ routes/get.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var jsonfile = require('jsonfile');
+var fs = require('fs');
 var router = express.Router();
 var helo = require('./helo');
+var json = fs.readFile('./views/ahooo.json',function(err){console.log(err);});
+console.log(json);
 router.get('/', function(req, res, next) {
-   res.json({
-        message:"Hello,world"
-    });
+  jsonfile.readFile('./views/ahooo.json',function(err,obj){
+  	if(err)console.error(err);
+  	res.json(obj);
+  });
 });
 module.exports = router;
