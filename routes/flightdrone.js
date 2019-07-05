d68d6688225818206e304da4dd7431f7ab3cf557
toki23 <NULL>
2019-07-05 21:21:29 +0900

    Wi-Fiの接続等の変更

--- routes/flightdrone.js
+++ routes/flightdrone.js
@@ -12,130 +12,125 @@ var c = new ftp();
 //↑モジュール呼び出してる
 
 router.get("/:number", function(req, res, next) {
-  if(req.params.number == "1"){
-  res.send("1");
-  }else{
-  res.send("0");
-  }
-  // wifi.init({
-  //   iface: null
-  // });
-  // wifi.disconnect(function(err) {
-  //   if (err) {
-  //     console.log(err);
-  //   }
-  //   console.log("Disconnected");
-  //   wifi.scan(function(err, networks) {
-  //     //wifiをスキャン
-  //     var conectedFlag = 0; //目的のドローンにアクセスできたかの変数
-  //     if (err) {
-  //       // wifiスキャンのエラー処理
-  //       console.log(err);
-  //     } else {
-  //       //   console.log(networks); // wifiの一覧をJSON形式で出力
-  //       for (var i = 0; i < Object.keys(networks).length; i++) {
-  //         //検索
-  //         //目的のwifiをサーチするfor文
-  //         if (networks[i].ssid == "Bebop2-043314") {
-  //           console.log("bebop2 there");
-  //           res.send("1");
-  //           conectedFlag = 1; //見つけたらFlagを1に
-  //           break;
-  //         }
-  //       }
-  //       if (conectedFlag == 0) {
-  //         res.send("0");
-  //         console.log("not found");
-  //       }
-  //       if (conectedFlag == 1) {
-  //         // jsonfile.readFile("./views/ahooo.json", function(err, obj) {
-  //         //   var a = "";
-  //         //   a += "QGC WPL 120\n";
-  //         //   a +=
-  //         //     "0	0	3	2500	0.000000	30.000000	2073600.000000	0.000000	0.000000	0.000000	0.000000	1\n";
-  //         //   a +=
-  //         //     "1	0	3	178	0.000000	6.000000	-1.000000	0.000000	0.000000	0.000000	0.000000	1\n";
-  //         //   a +=
-  //         //     "2	0	3	16	0.000000	5.000000	0.000000	" +
-  //         //     obj[req.params.number - 1].Lat +
-  //         //     " " +
-  //         //     obj[req.params.number - 1].Lng +
-  //         //     " 200 3.000000	1\n";
-  //         //   a +=
-  //         //     "3	0	3	50000	0.000000	-1.000000	0.000000	0.000000	0.000000	0.000000	0.000000	1\n";
-  //         //   a +=
-  //         //     "4	0	3	21	0.000000	0.000000	0.000000	" +
-  //         //     obj[req.params.number - 1].Lat +
-  //         //     " " +
-  //         //     obj[req.params.number - 1].Lng +
-  //         //     " 200 3.000000	1\n";
-  //         //   a +=
-  //         //     "5	0	3	2501	0.000000	0.000000	0.000000	0.000000	0.000000	0.000000	0.000000	1\n";
-  //         //   fs.writeFile("./routes/flightPlan.mavlink", a, function(err) {
-  //         //     console.log(err);
-  //         //   });
-  //         // });
-  //         wifi.connect({ ssid: "Bebop2-043314", password: "" }, function(err) {
-  //           if (err) {
-  //             console.log(err);
-  //           } else {
-  //             console.log("startConnect");
-  //             setTimeout(function() {
-  //               c.connect({
-  //                 host: "192.168.42.1",
-  //                 port: 21,
-  //                 user: "anonymous",
-  //                 password: ""
-  //               });
-  //               console.log("InsetTimeout");
-  //               c.on("ready", function() {
-  //                 console.log("ok");
-  //                 c.put(
-  //                   "./routes/flightPlan.mavlink",
-  //                   "internal_000/flightplans/flightPlan.mavlink",
-  //                   function(err) {
-  //                     if (err) throw err;
-  //                     console.log("putfinish");
-  //                     c.end();
-  //                   }
-  //                 );
-  //                 setTimeout(function() {
-  //                   drone.connect(function() {
-  //                     console.log("flightready");
-  //                     drone.Mavlink.start(
-  //                       "/data/ftp/internal_000/flightplans/flightPlan.mavlink",
-  //                       0
-  //                     );
-  //                   });
-  //                   setTimeout(function() {
-  //                     wifi.disconnect(function(err) {
-  //                       if (err) {
-  //                         console.log(err);
-  //                       }
-  //                       console.log("Disconnected");
-  //                     });
-  //                     setTimeout(function() {
-  //                       wifi.connect(
-  //                         { ssid: "media", password: "metro-cit" },
-  //                         function(err) {
-  //                           if (err) {
-  //                             console.log(err);
-  //                           }
-  //                           console.log("connected");
-  //                         }
-  //                       );
-  //                     }, 1000);
-  //                   }, 10000);
-  //                 }, 10000);
-  //               });
-  //             }, 10000);
-  //           }
-  //         });
-  //         console.log("foo");
-  //       }
-  //     }
-  //   });
-  // });
+  wifi.init({
+    iface: null
+  });
+  wifi.disconnect(function(err) {
+    if (err) {
+      console.log(err);
+    }
+    console.log("Disconnected");
+    wifi.scan(function(err, networks) {
+      //wifiをスキャン
+      var conectedFlag = 0; //目的のドローンにアクセスできたかの変数
+      if (err) {
+        // wifiスキャンのエラー処理
+        console.log(err);
+      } else {
+        //   console.log(networks); // wifiの一覧をJSON形式で出力
+        for (var i = 0; i < Object.keys(networks).length; i++) {
+          //検索
+          //目的のwifiをサーチするfor文
+          if (networks[i].ssid == "Bebop2-043314") {
+            console.log("bebop2 there");
+            res.send("1");
+            conectedFlag = 1; //見つけたらFlagを1に
+            break;
+          }
+        }
+        if (conectedFlag == 0) {
+          res.send("0");
+          console.log("not found");
+        }
+        if (conectedFlag == 1) {
+          jsonfile.readFile("./views/ahooo.json", function(err, obj) {
+            var a = "";
+            a += "QGC WPL 120\n";
+            a +=
+              "0	0	3	2500	0.000000	30.000000	2073600.000000	0.000000	0.000000	0.000000	0.000000	1\n";
+            a +=
+              "1	0	3	178	0.000000	6.000000	-1.000000	0.000000	0.000000	0.000000	0.000000	1\n";
+            a +=
+              "2	0	3	16	0.000000	5.000000	0.000000	" +
+              obj[req.params.number - 1].Lat +
+              " " +
+              obj[req.params.number - 1].Lng +
+              " 200 3.000000	1\n";
+            a +=
+              "3	0	3	50000	0.000000	-1.000000	0.000000	0.000000	0.000000	0.000000	0.000000	1\n";
+            a +=
+              "4	0	3	21	0.000000	0.000000	0.000000	" +
+              obj[req.params.number - 1].Lat +
+              " " +
+              obj[req.params.number - 1].Lng +
+              " 200 3.000000	1\n";
+            a +=
+              "5	0	3	2501	0.000000	0.000000	0.000000	0.000000	0.000000	0.000000	0.000000	1\n";
+            fs.writeFile("./routes/flightPlan.mavlink", a, function(err) {
+              console.log(err);
+            });
+          });
+          wifi.connect({ ssid: "Bebop2-043314", password: "" }, function(err) {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("startConnect");
+              setTimeout(function() {
+                c.connect({
+                  host: "192.168.42.1",
+                  port: 21,
+                  user: "anonymous",
+                  password: ""
+                });
+                console.log("InsetTimeout");
+                c.on("ready", function() {
+                  console.log("ok");
+                  c.put(
+                    "./routes/flightPlan.mavlink",
+                    "internal_000/flightplans/flightPlan.mavlink",
+                    function(err) {
+                      if (err) throw err;
+                      console.log("putfinish");
+                      c.end();
+                    }
+                  );
+                  setTimeout(function() {
+                    drone.connect(function() {
+                      console.log("flightready");
+                      drone.Mavlink.start(
+                        "/data/ftp/internal_000/flightplans/flightPlan.mavlink",
+                        0
+                      );
+                    });
+                    setTimeout(function() {
+                      wifi.disconnect(function(err) {
+                        if (err) {
+                          console.log(err);
+                        }
+                        console.log("Disconnected");
+                      });
+                      setTimeout(function() {
+                        wifi.connect(
+                          { ssid: "media", password: "metro-cit" },
+                          function(err) {
+                            if (err) {
+                              console.log(err);
+                            }
+                            console.log("connected");
+                          }
+                        );
+                      }, 1000);
+                    }, 10000);
+                  }, 10000);
+                });
+              }, 10000);
+            }
+          });
+          console.log("foo");
+        }
+      }
+    });
+  });
 });
 
 module.exports = router;
--- routes/getmovie.js
+++ routes/getmovie.js
@@ -4,70 +4,98 @@ var fs = require("fs");
 var ftp = require("ftp");
 var wifi = require("node-wifi");
 var router = express.Router();
+var archiver = require('archiver');
 var c = new ftp();
+var conectedFlag;
 
 //↑モジュール呼び出してる
 
 router.get("/", function(req, res, next) {
-//   wifi.init({
-//     iface: null
-//   });
-//   wifi.disconnect(function(err) {
-//     if (err) {
-//       console.log(err);
-//     }
-    console.log("Disconnected");
-   // wifi.scan(function(err, networks) {
-      //wifiをスキャン
-      var conectedFlag = 0; //目的のドローンにアクセスできたかの変数
-    
-        // wifiスキャンのエラー処理
-       // console.log(err);
-    
-        // //   console.log(networks); // wifiの一覧をJSON形式で出力
-        // for (var i = 0; i < Object.keys(networks).length; i++) {
-        //   //検索
-        //   //目的のwifiをサーチするfor文
-        //   if (networks[i].ssid == "Bebop2-043314") {
-        //     console.log("bebop2 there");
-        //     res.send("1");
-        //     conectedFlag = 1; //見つけたらFlagを1に
-        //     break;
-        //   }
-        // }
-        // if (conectedFlag == 0) {
-        //   res.send("0");
-        //   console.log("not found");
-        // }
-        // if (conectedFlag == 1) {
-         // wifi.connect({ ssid: "Bebop2-043314", password: "" }, function(err) {
-           // if (err) {
-             // console.log(err);
-            //} else {
-              console.log("startConnect");
+  wifi.init({
+    iface: null
+  });
+  wifi.disconnect(function(err) {
+    if (err) {
+      console.log(err);
+    }
+    wifi.scan(function(err, networks) {
+      console.log(err);
+      //   console.log(networks); // wifiの一覧をJSON形式で出力
+      for (var i = 0; i < Object.keys(networks).length; i++) {
+        //検索
+        //目的のwifiをサーチするfor文
+        if (networks[i].ssid == "Bebop2-043314") {
+          res.send("1");
+          conectedFlag = 1; //見つけたらFlagを1に
+          break;
+        }
+      }
+
+      if (conectedFlag == 0) {
+        res.send("0");
+        console.log("not found");
+      }
+      if (conectedFlag == 1) {
+        console.log("conect");
+        wifi.connect({ ssid: "Bebop2-043314", password: "" }, function(err) {
+          if (err) {
+            console.log(err);
+          } else {
+            setTimeout(function() {
               c.connect({
                 host: "192.168.42.1",
                 port: 21,
                 user: "anonymous",
                 password: ""
               });
-              console.log("InsetTimeout");
               c.on("ready", function() {
-                console.log("ok");
+                console.log("c.concect");
                 c.cwd("internal_000/Bebop_2/media", function(err, currentDir) {
                   c.list(function(err, list) {
                     if (err) throw err;
-                    console.dir(list);
+                    c.get(getFile(0, list), function(err, stream) {
+                      if (err) throw err;
+                      stream.once("close", function() {
+                        c.end();
+                      });
+                      stream.pipe(fs.createWriteStream("aa.mp4"));
+                    });
                     c.end();
+                    setTimeout(function(){
+                    wifi.disconnect(function(err) {
+                      if (err) {
+                        console.log(err);
+                      }
+                      wifi.connect(
+                        { ssid: "media", password: "metro-cit" },
+                        function(err) {
+
+                        }
+                      );
+                    });
+                  },50000);
                   });
                 });
               });
-           // }
-          //});
-        
-     // }
-//     });
-//   });
+            }, 50000);
+          }
+        });
+      }
+    });
+  });
 });
 
 module.exports = router;
+function getFile(i, H) {
+  H.sort(compare);
+  return H[i].name;
+}
+function compare(a, b) {
+  var r = 0;
+  if (a.date < b.date) {
+    r = 1;
+  } else if (a.date > b.date) {
+    r = -1;
+  }
+  return r;
+}
