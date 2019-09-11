69544312494e47e9c0ec38f031ca61ef2304c5c7
toki23 <NULL>
2019-09-11 11:19:45 +0900

    hsdfhdf

--- routes/helo.js
+++ routes/helo.js
@@ -31,7 +31,7 @@ async function writeGraphData(cowid,latitude,longitude){
     jsonfile.writeFile("./data_folder/cow_graph_data/cow"+cowid+".txt",file);
 }
 // /'* * * * *'
-cron.schedule('0 0 0,6,12,18 * * *',async  () => {
+cron.schedule('* * * * *',async  () => {
     console.log("start: cron");
     for(let i = 0;i<5;i++){
             const  dt = new Date();
