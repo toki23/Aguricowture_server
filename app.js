c3ed3e066b80f5da46addabe35678d803fadc8c8
toki23 <m18063@g.metro-cit.ac.jp>
2019-09-28 10:08:03 +0900

    he

--- app.js
+++ app.js
@@ -38,15 +38,12 @@ mqttClient();
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-});
-
-// CORSを許可する
-app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
+// CORSを許可する
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
--- routes/get.js
+++ routes/get.js
@@ -2,8 +2,9 @@ var express = require('express');
 var jsonfile = require('jsonfile');
 var router = express.Router();
 router.get('/', function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
   jsonfile.readFile('./views/ahooo.json',function(err,obj){
-  		res.json(obj);
+  res.json(obj);
   });
 });
 module.exports = router;
